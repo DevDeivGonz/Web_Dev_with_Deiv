95cf487d3d7e72f441f5d9c30d81035ea738d7f1
David <gonzdeiv123@gmail.com>
2024-08-12 17:28:17 -0500

    Add new files and update existing files

---
+++ 01_Learing ( HTML, CSS & JS )/03.1_JS/Basic Syntax/Variables and data types.js
@@ -0,0 +1,8 @@
+let a = 20;
+let b = 20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
--- 02_SENA_aprendizaje frontend y clases/Ejercicio 3/ejercicio3.js
+++ 02_SENA_aprendizaje frontend y clases/Ejercicio 3/ejercicio3.js
@@ -1,15 +1,22 @@
-function captura {
-    var nota1 = parseInt(document.getElementById("numero1").value);
-    var nota2 = parseInt(document.getElementById("numero2").value);
-    var nota3 = parseInt(document.getElementById("numero3").value);
-    let promedio = nota1 + nota2 + nota3 / 3;
+let intentos = 0;
+const maxIntentos = 5;
 
-    if ( promedio >= 7)
-        var texto = "Ha pasado";
-    else {
-        if (promedio > 4)
-            var texto = "Muy regular";
+function verificar_apuesta() {
+    let dado_1 = parseInt(document.getElementById("dado_1").value);
+    let dado_2 = parseInt(document.getElementById("dado_2").value);
+
+    if (intentos < maxIntentos) {
+        intentos++;
+
+        if (dado_1 + dado_2 === 7) {
+            console.log("Felicidades, ha ganado 5 puntos y $10000.");
+        } else if (dado_1 % 2 === 0 && dado_2 % 2 === 0) {
+            console.log("Felicidades, ha ganado 3 puntos y ha ganado $5000.");
+        } else if (dado_1 + dado_2 < 7) {
+            console.log(`Lo siento, ha perdido y le quedan ${maxIntentos - intentos} intentos.`);
+        }
+
+    } else {
+        console.log("Lo siento, ha alcanzado el número máximo de intentos.");
     }
-    document.getElementById("nc").value = texto;
-
 }
